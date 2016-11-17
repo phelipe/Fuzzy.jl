6dec47bd629d3584e9525f619aa2b5277305af66
phelipe <phelipewesleydeoliveira@gmail.com>
2016-11-17 12:45:06 -0300

    added T-norm

--- src/Fuzzy.jl
+++ src/Fuzzy.jl
@@ -1,17 +1,20 @@
 module Fuzzy
-	
+
 	export TriangularMF, GaussianMF, BellMF, TrapezoidalMF, SigmoidMF
-	
+
 	export Rule, FISMamdani, FISSugeno
-	
+
 	export eval_fis
-	
+
+	# T-Norm
+	include("T-norm.jl")
+
 	# Membership functions
 	include("MF.jl")
-	
+
 	# FIS
 	include("FIS.jl")
-	
+
 	# Evaluation functions
 	include("Eval.jl")
 
