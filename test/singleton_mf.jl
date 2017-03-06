a54dab15ebe7fbf5d91b9f9b391e23c96a5fc7c3
Phelipe <phelipewesleydeoliveira@gmail.com>
2017-03-06 12:45:10 -0300

    added tests

--- test/singleton_mf.jl
+++ test/singleton_mf.jl
@@ -7,4 +7,6 @@ mf = SingletonMF(center)
 @assert mf.eval(center - 1) == mf.eval(center + 1) == 0
 
 # Mean finding tests
-@assert mf.mean_at(1) == center
+@assert mf.mean_at(1.0) == center
+@assert mf.mean_at(0.5) == center
+@assert mf.mean_at(0.1) == center
