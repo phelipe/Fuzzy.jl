fc863e9333c6acfa83141bfdb83009aecfa471af
Phelipe Wesley <phelipewesleydeoliveira@gmail.com>
2021-07-18 12:43:21 -0300

    add more tests

--- test/fis_mamdani.jl
+++ test/fis_mamdani.jl
@@ -25,3 +25,31 @@ ival2 = [5.7, 2.0]
 
 @assert eval_fis(fis, ival1) == 2.0
 @assert eval_fis(fis, ival2) == 6.0
+
+
+service = Dict( "poor"      => TrapezoidalMF(0.0, 0.0, 2.0, 4.0), 
+                "good"      => TriangularMF(3., 5., 7.),
+                "excellent" => TrapezoidalMF(6., 8., 10., 10.))
+
+food = Dict("rancid"    => TrapezoidalMF(0., 0., 3., 6.),
+            "delicious" => TrapezoidalMF(4., 7., 10., 10.))
+
+inputs = [service, food]
+
+tip  = Dict("cheap"     => TrapezoidalMF(10., 10., 20., 30.),
+            "average"   => TriangularMF(20., 30., 40.), 
+            "generous"  => TrapezoidalMF(30., 40, 50., 50.))
+
+rule1 = Rule(["poor", "rancid"], "cheap", "MAX")
+
+# How to not include food input ?
+rule2 = Rule(["good", ""], "average", "MAX")
+
+rule3 = Rule(["excellent", "delicious"], "generous", "MAX")
+rules = [rule1, rule2, rule3]
+
+fis_tips = FISMamdani(inputs, tip, rules)
+
+in_vals = [7., 8,]
+
+@assert eval_fis(fis_tips, in_vals) == 45.0
