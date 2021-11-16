d513e664e98794fa252538535216519048bf209c
Phelipe Wesley <phelipewesleydeoliveira@gmail.com>
2021-11-16 09:02:47 -0300

    create DotsMF file

---
+++ src/DotsMF.jl
@@ -0,0 +1,20 @@
+get_dots(membership_function::MF, input_point_vector::Union{StepRange,StepRangeLen}) =
+    get_dots(membership_function, collect(input_point_vector))
+
+function get_dots(membership_function::MF, input_point_vector::Vector{T}) where {T<:Number}
+    return Real.(map(x -> Fuzzy.eval(membership_function, x), input_point_vector))
+end
+
+chart_prepare(sets_dict::Dict{String,N}, input_point_vector::Union{StepRange,StepRangeLen}) where {N<:MF} =
+    chart_prepare(sets_dict, collect(input_point_vector))
+
+function chart_prepare(sets_dict::Dict{String,N}, input_point_vector::Vector{T}) where {T<:Number,N<:MF}
+    msize = length(sets_dict)
+    names = String[]
+    outputs = Array{T,1}[]
+    for (name, membership_function) in sets_dict
+        push!(names, name)
+        push!(outputs, get_dots(membership_function, input_point_vector))
+    end
+    return Dict("names" => names, "values" => outputs)
+end
